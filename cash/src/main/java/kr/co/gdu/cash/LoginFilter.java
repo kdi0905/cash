f3ac26d70471212ecd7f611637d94865057ad44a
CTU <CTU@192.168.123.100>
2020-11-05 11:41:01 +0900

    전 commit 추가

---
+++ cash/src/main/java/kr/co/gdu/cash/LoginFilter.java
@@ -0,0 +1,30 @@
+package kr.co.gdu.cash;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+//필터 implements Filter추가
+@WebFilter(urlPatterns = "/admin/*") //필터 가로체기
+public class LoginFilter implements Filter{
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+	HttpSession session=((HttpServletRequest)request).getSession();
+	if(session.getAttribute("loginId")==null) {
+		((HttpServletResponse)response).sendRedirect("/login");
+		return;
+	}
+	chain.doFilter(request, response);
+		
+	}
+
+}
