2a3f4da998f56a70296f1d27aa02e6843f544e93
CTU <CTU@192.168.123.100>
2020-11-05 11:40:33 +0900

    로그인에 사용할 member vo,Mapper,service,filter ,db 작업

--- cash/src/main/java/kr/co/gdu/cash/controller/LoginController.java
+++ cash/src/main/java/kr/co/gdu/cash/controller/LoginController.java
@@ -2,7 +2,6 @@ package kr.co.gdu.cash.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +9,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import kr.co.gdu.cash.service.MemberService;
 import kr.co.gdu.cash.service.NoticeService;
+import kr.co.gdu.cash.vo.Member;
 import kr.co.gdu.cash.vo.Notice;
 
 @Controller
 public class LoginController {
-	@Autowired
-	private NoticeService noticeService;
+	@Autowired private NoticeService noticeService;
+	@Autowired private MemberService memberService;
 	//자바 관련 코드 타입이름을 같이 표현하는???방식
 	//카멜방식
 	//파스클 방식 - class
@@ -36,11 +36,18 @@ public class LoginController {
 	}
 	
 	@PostMapping("/login")
-	public String login(@RequestParam(value="id")String id) { // 로그인 액션
-		return"";
+	public String login(Member member, HttpSession session
+			//@RequestParam(value="id")String id,@RequestParam(value="pw")String pw
+			) { // 로그인 액션
+		Member loginMember= memberService.getMemberById(member);
+		if(loginMember == null) {
+			return"redirect:/login";
+		}
+		session.setAttribute("loginId", loginMember.getId());
+		return"redirect:/admin/index";
 	}
 	@GetMapping("/admin/logout")
-	public String logout(HttpSession session) { //로그인할때만 사용해서 간편하게 사용 //tnldnsqkdqjq
+	public String logout(HttpSession session) { //로그인할때만 사용해서 간편하게 사용 //쉬운방법
 		session.invalidate();
 		return"redirect:/";
 	}
--- cash/src/main/webapp/WEB-INF/view/login.jsp
+++ cash/src/main/webapp/WEB-INF/view/login.jsp
@@ -1,5 +1,5 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +7,38 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+<div>
+		<!-- 공지 -->
+		<table border="1">
+		<thead>
+			<tr>
+				<th>notice_id</th>
+				<th>notice_title</th>
+				<th>notice_content</th>
+			</tr>
+		</thead>
+		<tbody>
+		<c:forEach var ="n" items="${noticeList}">
+			<tr>
+				<td>${n.noticeId }</td>
+				<td>${n.noticeTitle }</td>
+				<td>${n.noticeContent }</td>
+			</tr>
+		</c:forEach>
+		</tbody>
+		</table>
+	</div>
+	<h1>로그인</h1>
+	<form method="post" action="/login">
+		<div>
+			ID: <input type ="text" name="id">
+		</div>
+		<div>
+			PW: <input type ="password" name="pw">
+		</div>
+		<div>
+			<button type="submit">로그인</button>
+		</div>
+	</form>
 </body>
 </html>
