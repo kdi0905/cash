9b2bf731ee75bb732fc6bb4eaca45fb71fd64aea
GDJ29 <GDJ29@DESKTOP-DSD77UN>
2020-11-06 14:43:33 +0900

    cash.sql 추가

--- cash/src/main/webapp/WEB-INF/view/cash.sql
+++ cash/src/main/webapp/WEB-INF/view/cash.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
 -- 호스트:                          127.0.0.1
--- 서버 버전:                        10.5.5-MariaDB - mariadb.org binary distribution
+-- 서버 버전:                        10.3.24-MariaDB - mariadb.org binary distribution
 -- 서버 OS:                        Win64
 -- HeidiSQL 버전:                  11.0.0.5919
 -- --------------------------------------------------------
@@ -31,17 +31,16 @@ CREATE TABLE IF NOT EXISTS `cashbook` (
   PRIMARY KEY (`cashbook_id`),
   KEY `FK_cashbook_category` (`category_name`),
   CONSTRAINT `FK_cashbook_category` FOREIGN KEY (`category_name`) REFERENCES `category` (`category_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
 
 -- 테이블 데이터 cash.cashbook:~8 rows (대략적) 내보내기
 /*!40000 ALTER TABLE `cashbook` DISABLE KEYS */;
 INSERT INTO `cashbook` (`cashbook_id`, `cashbook_kind`, `category_name`, `cashbook_price`, `cashbook_content`, `cashbook_date`, `create_date`, `update_date`) VALUES
-	(1, '수입', '급여', 2000000, '급여:수입', '2020-11-02', '2020-11-04 09:38:39', '2020-11-04 09:38:40'),
-	(2, '수입', '용돈', 100000, '용돈:수입', '2020-11-03', '2020-11-04 09:39:35', '2020-11-04 09:39:36'),
-	(3, '지출', '생활', 50000, '생활:지출', '2020-11-03', '2020-11-04 09:40:28', '2020-11-04 09:40:29'),
-	(8, '지출', '식비', 10000, '식비:지출', '2020-11-03', '2020-11-04 09:41:23', '2020-11-04 09:41:24'),
+	(1, '수입', '급여', 2000000, '급여', '2020-11-02', '2020-11-04 09:38:39', '2020-11-04 09:38:40'),
+	(2, '수입', '용돈', 100001, '용돈', '2020-11-03', '2020-11-04 09:39:35', '2020-11-06 13:53:24'),
+	(3, '지출', '생활', 50000, '생활', '2020-11-03', '2020-11-04 09:40:28', '2020-11-04 09:40:29'),
+	(8, '지출', '식비', 10000, '식비', '2020-11-03', '2020-11-04 09:41:23', '2020-11-04 09:41:24'),
 	(9, '수입', '급여', 10000000, '퇴직금', '2020-11-04', '2020-11-04 15:54:55', '2020-11-04 15:54:55'),
-	(10, '지출', '식비', 5000, '점심', '2020-11-03', '2020-11-04 16:03:01', '2020-11-04 16:03:01'),
 	(11, '지출', '식비', 7000, '저녁', '2020-11-03', '2020-11-04 16:04:02', '2020-11-04 16:04:02'),
 	(12, '수입', '용돈', 10000, '용돈', '2020-11-03', '2020-11-04 16:04:44', '2020-11-04 16:04:44');
 /*!40000 ALTER TABLE `cashbook` ENABLE KEYS */;
@@ -54,11 +53,12 @@ CREATE TABLE IF NOT EXISTS `category` (
   PRIMARY KEY (`category_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- 테이블 데이터 cash.category:~6 rows (대략적) 내보내기
+-- 테이블 데이터 cash.category:~5 rows (대략적) 내보내기
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
 INSERT INTO `category` (`category_name`, `category_info`) VALUES
 	('관광', '지출'),
 	('급여', '수입'),
+	('기타', '수입/지출'),
 	('생활', '지출'),
 	('식비', '지출'),
 	('용돈', '수입'),
@@ -73,7 +73,7 @@ CREATE TABLE IF NOT EXISTS `member` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 테이블 데이터 cash.member:~0 rows (대략적) 내보내기
+-- 테이블 데이터 cash.member:~1 rows (대략적) 내보내기
 /*!40000 ALTER TABLE `member` DISABLE KEYS */;
 INSERT INTO `member` (`id`, `pw`) VALUES
 	('admin', '1234');
@@ -89,7 +89,7 @@ CREATE TABLE IF NOT EXISTS `notice` (
   PRIMARY KEY (`notice_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4;
 
--- 테이블 데이터 cash.notice:~0 rows (대략적) 내보내기
+-- 테이블 데이터 cash.notice:~1 rows (대략적) 내보내기
 /*!40000 ALTER TABLE `notice` DISABLE KEYS */;
 INSERT INTO `notice` (`notice_id`, `notice_title`, `notice_content`, `notice_date`) VALUES
 	(3, 'To do list', '1. cashbook날짜별 CRUD //수정 삭제 추가\r\n2.notice CRUD\r\n3.cagegory CRUD\r\n3. 통계페이지(년별 지출합, 수입합, 카테고리별,...)', '2020-11-04 13:40:35'),
